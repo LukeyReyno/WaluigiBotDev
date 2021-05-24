783629f41ffec9dfe94765aa8fb48d275aea41ad
Lucas Reyna <lucas.reyna143@gmail.com>
2021-05-23 21:03:58 -0700

    Improvements to wc and xxd

--- commands/exec.py
+++ commands/exec.py
@@ -1,22 +1,43 @@
 import discord
 import os
-import requests
-import WaluigiBotDev
+import subprocess
 
 from discord.ext import commands, tasks
 from json import *
 
-WCFILE = "data/wordcount.txt"
-XXDFILE = "data/xxd.txt"
+DISCORD_MESSAGE_CHAR_LIMIT = 2000
+MESSAGEHISTORYLIMIT = 35
 
 async def getMostRecentFile(ctx):
-    messages = await ctx.channel.history(limit=45).flatten()
+    messages = await ctx.channel.history(limit=MESSAGEHISTORYLIMIT).flatten()
     j = 0
     while (j < len(messages)):
         if (len(messages[j].attachments) > 0):
             return messages[j].attachments[0]
         j += 1
     return None
+
+async def basicExecWithFile(ctx, command):
+    messageAttachment = await getMostRecentFile(ctx)
+    if (messageAttachment != None):
+        outputPath = f"exec/{command}Result.txt"
+
+        fname = f"exec/{messageAttachment.filename}"
+        await messageAttachment.save(fname)
+        stdOutput = subprocess.Popen([command, fname], stdout=subprocess.PIPE).communicate()[0]
+        stdOutput = stdOutput.decode('utf-8')
+        if (len(stdOutput) >= DISCORD_MESSAGE_CHAR_LIMIT):
+            outfile = open(outputPath, 'w')
+            outfile.write(stdOutput)
+            outfile.close()
+            destinationFile = discord.File(f"{outputPath}")
+            await ctx.send(f"`{command.toUpper()} file: `", file=destinationFile)
+            os.remove(f"{outputPath}")
+        else:
+            await ctx.send(f"```{stdOutput}```")
+        os.remove(fname)
+    else:
+        return await ctx.send(f"`No recent files (Last {MESSAGEHISTORYLIMIT} messages). Send a new one.`")
 
 class exec(commands.Cog):
 
@@ -25,33 +46,11 @@ class exec(commands.Cog):
 
     @commands.command(aliases=["wc"])
     async def wordcount(self, ctx):
-        messageAttachment = await getMostRecentFile(ctx)
-        if (messageAttachment != None):
-            print(messageAttachment.content_type)
-            fname = f"data/{messageAttachment.filename}"
-            await messageAttachment.save(fname)
-            exitcode = os.system(f"wc {fname} > {WCFILE}")
-            destinationFile = discord.File(f"{WCFILE}")
-            await ctx.send("`WordCount File: `", file=destinationFile)
-            os.remove(fname)
-            os.remove(f"{WCFILE}")
-        else:
-            await ctx.send("`No recent files were sent in this chat.`")
+        await basicExecWithFile(ctx, "wc")
 
     @commands.command(aliases=["hex"])
     async def xxd(self, ctx):
-        messageAttachment = await getMostRecentFile(ctx)
-        if (messageAttachment != None):
-            print(messageAttachment.content_type)
-            fname = f"data/{messageAttachment.filename}"
-            await messageAttachment.save(fname)
-            exitcode = os.system(f"xxd {fname} > {XXDFILE}")
-            destinationFile = discord.File(f"{XXDFILE}")
-            await ctx.send("`XXD file: `", file=destinationFile)
-            os.remove(fname)
-            os.remove(f"{XXDFILE}")
-        else:
-            await ctx.send("`No recent files were sent in this chat.`")
+        await basicExecWithFile(ctx, "xxd")
         
 
 def setup(client):
